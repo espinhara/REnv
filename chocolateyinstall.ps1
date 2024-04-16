903dc252c3fd3416e4b4d41abb02f6d000c8922f
Gabriel Espinhara da Silva <52253458+espinhara@users.noreply.github.com>
2024-04-16 12:16:19 -0300

    Update chocolateyinstall.ps1

--- chocolateyinstall.ps1
+++ chocolateyinstall.ps1
@@ -1,4 +1,8 @@
-$profilePath = $PROFILE.CurrentUserAllHosts
-Add-Content -Path $profilePath -Value "`r`n. '$env:USERPROFILE\refreshenv.ps1'" -Force
+# Define a URL do arquivo refreshenv.ps1 no seu repositório do GitHub
+$refreshEnvScriptUrl = "https://raw.githubusercontent.com/espinhara/RefreshEnv/master/refreshenv.ps1"
 
+# Baixa o arquivo refreshenv.ps1 para o diretório temporário
+Invoke-WebRequest -Uri $refreshEnvScriptUrl -OutFile "$env:TEMP\refreshenv.ps1" -UseBasicParsing
 
+# Copia o script refreshenv.ps1 para o diretório de instalação do Chocolatey
+Copy-Item "$env:TEMP\refreshenv.ps1" "$env:USERPROFILE\refreshenv.ps1" -Force
